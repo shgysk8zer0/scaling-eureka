798bdea2910852ad7a751e9f6a2a7eedbf74bad7
Chris Zuber <shgysk8zer0@gmail.com>
2019-08-22 11:22:38 -0700

    Update org metadata to be specific instead of generic

--- components/schema/trip.js
+++ components/schema/trip.js
@@ -35,7 +35,7 @@ customElements.define('schema-trip', class SchemaTripElement extends HTMLFormEle
 			this.addEventListener('submit', async event => {
 				event.preventDefault();
 				console.log(JSON.parse(JSON.stringify(this)));
-				navigator.clipboard.writeText(JSON.stringify(this));
+				navigator.clipboard.writeText(JSON.stringify(this, null, 4));
 			})
 		}).catch(console.error);
 	}
@@ -74,41 +74,74 @@ customElements.define('schema-trip', class SchemaTripElement extends HTMLFormEle
 			description: this.get('description'),
 			provider: {
 				'@type': 'Organization',
+				name: 'SoCal Rafting',
+				url: 'https://www.socalrafting.com/',
+				email: 'booking@socalrafting.com',
+				telephone: '+1-888-537-6748',
+				sameAs: [
+					'http://www.facebook.com/kernriverrafting',
+					'https://www.yelp.com/biz/socal-rafting-kernville',
+					'http://instagram.com/kernriverrafting',
+					'http://www.youtube.com/c/socalrafting',
+				],
+				logo: {
+					'@type': 'ImageObject',
+					url: 'https://static.wixstatic.com/media/47074e_e3fd372c95924fc185ce4e614f46669f~mv2_d_2768_1384_s_2.png/v1/crop/x_0,y_264,w_2768,h_863/fill/w_304,h_95,al_c,q_80,usm_0.66_1.00_0.01/47074e_e3fd372c95924fc185ce4e614f46669f~mv2_d_2768_1384_s_2.png',
+					height: 95,
+					width: 304
+				},
+				image: {
+					'@type': 'ImageObject',
+					url: 'https://static.wixstatic.com/media/47074e_fc91557ac96b476aa9487870836a515d~mv2.jpg/v1/fill/w_636,h_477,al_c,q_80,usm_0.66_1.00_0.01/47074e_fc91557ac96b476aa9487870836a515d~mv2.jpg',
+					height: 477,
+					width: 636
+				},
 				location: {
 					'@type': 'Place',
-					name: 'Acme Rafting Co.',
+					name: 'SoCal Rafting',
 					address: {
 						'@type': 'PostalAddress',
-						streetAddress: '123 Some St.',
-						addressLocality: 'Nowhere',
+						streetAddress: '11101 Kernville Rd.',
+						addressLocality: 'Kernville',
 						addressRegion: 'CA',
 						addressCountry: 'US',
+						postalCode: 93238
 					},
 					openingHoursSpecification: [{
 						'@type': 'OpeningHoursSpecification',
+						dayOfWeek: 'Sunday',
+						opens: '08:00',
+						closes: '20:00',
+					}, {
+						'@type': 'OpeningHoursSpecification',
 						dayOfWeek: 'Monday',
-						opens: '09:00-07:00',
-						closes: '17:00-07:00',
+						opens: '08:00',
+						closes: '18:00',
 					}, {
 						'@type': 'OpeningHoursSpecification',
 						dayOfWeek: 'Tuesday',
-						opens: '09:00-07:00',
-						closes: '17:00-07:00',
+						opens: '08:00',
+						closes: '18:00',
 					}, {
 						'@type': 'OpeningHoursSpecification',
 						dayOfWeek: 'Wednesday',
-						opens: '09:00-07:00',
-						closes: '17:00-07:00',
+						opens: '08:00',
+						closes: '18:00',
 					}, {
 						'@type': 'OpeningHoursSpecification',
 						dayOfWeek: 'Thursday',
-						opens: '09:00-07:00',
-						closes: '17:00-07:00',
+						opens: '08:00',
+						closes: '18:00',
 					}, {
 						'@type': 'OpeningHoursSpecification',
 						dayOfWeek: 'Friday',
-						opens: '09:00-07:00',
-						closes: '17:00-07:00',
+						opens: '08:00',
+						closes: '20:00',
+					}, {
+						'@type': 'OpeningHoursSpecification',
+						dayOfWeek: 'Saturday',
+						opens: '08:00',
+						closes: '20:00',
 					}],
 				},
 			},
